f1703f15c2230da55e3ebebf26dcc1c5cac43603
Rigoberto Sáenz Imbacuán <beto456789@gmail.com>
2017-01-21 19:37:11 -0500

    - Icons added - Basic screen templates added

---
+++ MyAppCatalog/MyAppCatalog/ScreenAppList.swift
@@ -0,0 +1,14 @@
+//
+//  ScreenAppList.swift
+//  MyAppCatalog
+//
+//  Created by Rigoberto Sáenz Imbacuán on 1/21/17.
+//  Copyright © 2017 Rigoberto Sáenz Imbacuán. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ScreenAppList: UIViewController {
+    
+}
---
+++ MyAppCatalog/MyAppCatalog/ScreenCatalog.swift
@@ -0,0 +1,14 @@
+//
+//  ScreenCatalog.swift
+//  MyAppCatalog
+//
+//  Created by Rigoberto Sáenz Imbacuán on 1/21/17.
+//  Copyright © 2017 Rigoberto Sáenz Imbacuán. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ScreenCatalog: UIViewController {
+    
+}
